08bbc3c23fbe24b5edd2ff8aea1504017bf0fbc2
Matthew Allwright <matt.a.4890@gmail.com>
2019-11-10 15:24:44 -0700

    Added basic regexes

--- CourseSchedulerAI/src/parser/Parser.java
+++ CourseSchedulerAI/src/parser/Parser.java
@@ -12,12 +12,14 @@ class Parser {
 
     public static void main(String[] args) {
 
+        // Setup file params
         String filename = "./CourseSchedulerAI/inputs/shortExample.txt";
-        ArrayList<String> lines = new ArrayList<String>();
+        ArrayList<String> fileLines = new ArrayList<String>();
 
+        // Read in the file
         try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
             while (br.ready()) {
-                lines.add(br.readLine());
+                fileLines.add(br.readLine());
             }
         } catch (FileNotFoundException fnfe) {
             System.out.println("File not found: " + filename);
@@ -28,6 +30,14 @@ class Parser {
             System.out.println("Exiting...");
             System.exit(1);
         }
+
+        // Setup regular expressions
+        Pattern slotRegex   = Pattern.compile("(MO|TU|FR),\\s*(\\d{1,2}:\\d{2}),\\s*(\\d+),\\s*(\\d+)");
+        Pattern courseRegex = Pattern.compile("([A-Z]{4})\\s*(\\d{3})\\s*LEC\\s*(\\d{2})");
+        Pattern labRegex    = Pattern.compile("([A-Z]{4})\\s*(\\d{3})\\s*(?:LAB|TUT)\\s*(\\d{2})");
+        Pattern labLecRegex = Pattern.compile("([A-Z]{4})\\s*(\\d{3})\\s*LEC\\s*(\\d{2})\\s*(?:LAB|TUT)\\s*(\\d{2})");
+
+        // Iterate through file lines
 
     }
 
