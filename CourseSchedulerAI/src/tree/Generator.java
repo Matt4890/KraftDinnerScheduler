bbf22e4e322402b05bbf367a97eb98df31cf7608
kaitlin.decf <kaitlin.decf@gmail.com>
2019-11-15 19:11:06 -0700

    fixes

--- CourseSchedulerAI/src/tree/Generator.java
+++ CourseSchedulerAI/src/tree/Generator.java
@@ -6,26 +6,37 @@ import coursesULabs.*;
 import java.util.*;
 
 public class Generator {
+    private Tree tree;
+    private Schedule starter;
+    private int initialPenalty;
+    private int bound; 
+    public Generator (Schedule starter, int penalty){
+        this.tree = new Tree();
+        this.starter = starter;
+        this.initialPenalty = penalty;
+        this.bound = Integer.MAX_VALUE;
 
-    public int createFBound(Schedule startingScheduleFromParser, int initialPenaltyValueFromParser,
-            ArrayList<Unit> toBeAdded) {
-        Tree tree = new Tree();
-        tree.addRoot(new Node(startingScheduleFromParser, initialPenaltyValueFromParser));
-        Node lastNode = tree.getRoot();
+    }
+    public void createFBound( ArrayList<Unit> toBeAdded) {
+        
+        this.tree.addRoot(new Node(this.starter, this.initialPenalty));
+        Node lastNode = this.tree.getRoot();
+    
         for (int i = 0; i < toBeAdded.size(); i++) {
             // Check what type of unit it is and then create the nodes accordingly
             Unit current = toBeAdded.get(i);
+            int minimum = Integer.MAX_VALUE;
             if (current instanceof Course) {
-                int minimum = Integer.MAX_VALUE;
+            
                 int idx = Integer.MAX_VALUE;
                 int day = -1;
-                for (Map.Entry<Integer, Slot> entry : startingScheduleFromParser.getMWFLec().entrySet()) {
+                for (Map.Entry<Integer, Slot> entry : this.starter.getMWFLec().entrySet()) {
                     // Calculate the penalty of the course slot pairing
                     int calc = SoftConstraints.calculatePenalty(entry.getValue(), current);
                     if (calc < minimum && HardConstrainsts.checkAssignmentHardConstriantsCourse((Course) current,
-                            (CourseSlot) entry.getValue(), startingScheduleFromParser.getMWFLec(),
-                            startingScheduleFromParser.getTuThLec(), startingScheduleFromParser.getMWLab(),
-                            startingScheduleFromParser.getTuThLab(), startingScheduleFromParser.getFLab())) {
+                            (CourseSlot) entry.getValue(), this.starter.getMWFLec(),
+                            this.starter.getTuThLec(), this.starter.getMWLab(),
+                            this.starter.getTuThLab(), this.starter.getFLab())) {
                         // Run a hard constraint check
 
                         calc = minimum;
@@ -33,13 +44,13 @@ public class Generator {
                         day = 0;
                     }
                 }
-                for (Map.Entry<Integer, Slot> entry : startingScheduleFromParser.getTuThLec().entrySet()) {
+                for (Map.Entry<Integer, Slot> entry : this.starter.getTuThLec().entrySet()) {
                     int calc = SoftConstraints.calculatePenalty(entry.getValue(), current);
                     // Check if we pass the hard constraints and its a new minimal
                     if (calc < minimum && HardConstrainsts.checkAssignmentHardConstriantsCourse((Course) current,
-                            (CourseSlot) entry.getValue(), startingScheduleFromParser.getMWFLec(),
-                            startingScheduleFromParser.getTuThLec(), startingScheduleFromParser.getMWLab(),
-                            startingScheduleFromParser.getTuThLab(), startingScheduleFromParser.getFLab())) {
+                            (CourseSlot) entry.getValue(), this.starter.getMWFLec(),
+                            this.starter.getTuThLec(), this.starter.getMWLab(),
+                            this.starter.getTuThLab(), this.starter.getFLab())) {
                         calc = minimum;
                         idx = entry.getKey();
                         day = 1;
@@ -47,26 +58,25 @@ public class Generator {
                 }
                 // Assign the course to the slot
                 if (day == 0) { // MondayWednesdayFriday
-                    startingScheduleFromParser.getMWFLec().get(idx).assignUnitToSlot(current);
+                    this.starter.getMWFLec().get(idx).assignUnitToSlot(current);
                 } else { // TuesdayThursday
-                    startingScheduleFromParser.getTuThLec().get(idx).assignUnitToSlot(current);
+                    this.starter.getTuThLec().get(idx).assignUnitToSlot(current);
                 }
                 // Not Sure if this should be a deep copy or a shallow reference is okay.
-                Node newCreatedNode =new Node(startingScheduleFromParser, minimum + lastNode.getPenaltyValueOfNode(), lastNode);
+                Node newCreatedNode =new Node(this.starter, minimum + lastNode.getPenaltyValueOfNode(), lastNode);
                 lastNode.addChild(newCreatedNode);
                 lastNode = newCreatedNode;
 
             } else {
-                int minimum = Integer.MAX_VALUE;
                 int idx = Integer.MAX_VALUE;
                 int day = -1;
                 // Check all the lab slot assignments that would be best
-                for (Map.Entry<Integer, Slot> entry : startingScheduleFromParser.getMWLab().entrySet()) {
+                for (Map.Entry<Integer, Slot> entry : this.starter.getMWLab().entrySet()) {
                     int calc = SoftConstraints.calculatePenalty(entry.getValue(), current);
                     if (calc < minimum && HardConstrainsts.checkAssignmentHardConstriantsLab((Lab) current,
-                            (LabSlot) entry.getValue(), startingScheduleFromParser.getMWFLec(),
-                            startingScheduleFromParser.getTuThLec(), startingScheduleFromParser.getMWLab(),
-                            startingScheduleFromParser.getTuThLab(), startingScheduleFromParser.getFLab())) {
+                            (LabSlot) entry.getValue(), this.starter.getMWFLec(),
+                            this.starter.getTuThLec(), this.starter.getMWLab(),
+                            this.starter.getTuThLab(), this.starter.getFLab())) {
 
                         calc = minimum;
                         idx = entry.getKey();
@@ -74,12 +84,12 @@ public class Generator {
                     }
 
                 }
-                for (Map.Entry<Integer, Slot> entry : startingScheduleFromParser.getTuThLab().entrySet()) {
+                for (Map.Entry<Integer, Slot> entry : this.starter.getTuThLab().entrySet()) {
                     int calc = SoftConstraints.calculatePenalty(entry.getValue(), current);
                     if (calc < minimum && HardConstrainsts.checkAssignmentHardConstriantsLab((Lab) current,
-                            (LabSlot) entry.getValue(), startingScheduleFromParser.getMWFLec(),
-                            startingScheduleFromParser.getTuThLec(), startingScheduleFromParser.getMWLab(),
-                            startingScheduleFromParser.getTuThLab(), startingScheduleFromParser.getFLab())) {
+                            (LabSlot) entry.getValue(), this.starter.getMWFLec(),
+                            this.starter.getTuThLec(), this.starter.getMWLab(),
+                            this.starter.getTuThLab(), this.starter.getFLab())) {
 
                         calc = minimum;
                         idx = entry.getKey();
@@ -87,12 +97,12 @@ public class Generator {
                     }
 
                 }
-                for (Map.Entry<Integer, Slot> entry : startingScheduleFromParser.getFLab().entrySet()) {
+                for (Map.Entry<Integer, Slot> entry : this.starter.getFLab().entrySet()) {
                     int calc = SoftConstraints.calculatePenalty(entry.getValue(), current);
                     if (calc < minimum && HardConstrainsts.checkAssignmentHardConstriantsLab((Lab) current,
-                            (LabSlot) entry.getValue(), startingScheduleFromParser.getMWFLec(),
-                            startingScheduleFromParser.getTuThLec(), startingScheduleFromParser.getMWLab(),
-                            startingScheduleFromParser.getTuThLab(), startingScheduleFromParser.getFLab())) {
+                            (LabSlot) entry.getValue(), this.starter.getMWFLec(),
+                            this.starter.getTuThLec(), this.starter.getMWLab(),
+                            this.starter.getTuThLab(), this.starter.getFLab())) {
 
                         calc = minimum;
                         idx = entry.getKey();
@@ -100,19 +110,20 @@ public class Generator {
                     }
                 }
                 if (day == 0) { // MondayWednesday
-                    startingScheduleFromParser.getMWLab().get(idx).assignUnitToSlot(current);
+                    this.starter.getMWLab().get(idx).assignUnitToSlot(current);
                 } else if (day == 1) { // TuesdayThursday
-                    startingScheduleFromParser.getTuThLab().get(idx).assignUnitToSlot(current);
+                    this.starter.getTuThLab().get(idx).assignUnitToSlot(current);
                 } else { //Friday
-                    startingScheduleFromParser.getFLab().get(idx).assignUnitToSlot(current);
+                    this.starter.getFLab().get(idx).assignUnitToSlot(current);
                 }
-                Node newCreatedNode =new Node(startingScheduleFromParser, minimum + lastNode.getPenaltyValueOfNode(), lastNode);
+                Node newCreatedNode =new Node(this.starter, minimum + lastNode.getPenaltyValueOfNode(), lastNode);
                 lastNode.addChild(newCreatedNode);
                 lastNode = newCreatedNode;
 
             }
+            this.bound +=minimum;
+            System.out.println(bound);
         }
-        return 0;
     }
-    
+
 }
