92fff4bc8c17e480cb790569ec8a65fd09f4f3c8
DenisShev <shevdenis1835@gmail.com>
2019-11-21 15:00:53 -0700

    .

--- CourseSchedulerAI/src/Main.java
+++ CourseSchedulerAI/src/Main.java
@@ -1,21 +1,30 @@
+import parser.*;
+import schedule.*;
+import tree.*;
+
 class main{
   public static void main(String[] args){
 
-    string filename;
+    String filename;
     int courseMinPen;
     int pairsPen;
     int brothersPen;
 
 
-    filename = args[0];
-    courseMinPen = args[1];
-    pairsPen = args[2];
-    brothersPen = args[3];
 
-    Parser parser = new Parser;
+
+    filename = args[0];
+    courseMinPen = Integer.parseInt(args[1]);
+    pairsPen = Integer.parseInt(args[2]);
+    brothersPen = Integer.parseInt(args[3]);
+
+    Parser parser = new Parser();
     parser.parseInput(filename, courseMinPen, pairsPen, brothersPen);
 
-    Generator gen = new Generator;
+    Schedule schedule = new Schedule(parser.partialAssignments, parser.courseSlots, parser.labSlots);
+
+    //Pen not gonna stay at 0. Ask Kaitlyn what it'll be.
+    Generator gen = new Generator(schedule, 0);
     gen.createFBound(parser.getMap());
 
 
