761eeef0fc5e89a45bfc5fca07d6f6afd007c3dc
julio.agostini <julio.agostini@cs.ucalgary.ca>
2019-11-22 13:19:42 -0700

    more refactor hop i didnt break anything

--- CourseSchedulerAI/src/Constraints/HardConstrainsts.java
+++ CourseSchedulerAI/src/Constraints/HardConstrainsts.java
@@ -9,71 +9,58 @@ import enums.*;
 
 public class HardConstrainsts {
 
-    public static boolean checkAssignmentHardConstriantsCourse(final Course course, final CourseSlot slot,
-            final HashMap<Integer, Slot> MWFLec, final HashMap<Integer, Slot> TuThLec,
-            final HashMap<Integer, Slot> MWLab, final HashMap<Integer, Slot> TuThLab,
-            final HashMap<Integer, Slot> FLab) {
+    public static boolean checkAssignmentHardConstriantsCourse(Course course, CourseSlot slot) {
         return checkCourseMax(course, slot) && checkDuplicateCourse(course, slot) && checkEveningClasses(course, slot)
                 && checkConflict500Levels(course, slot)
-                && checkLabConflicts(course, slot, MWFLec, TuThLec, MWLab, TuThLab, FLab);
+                && checkConflictsWithSameName(course, slot);
     }
 
-    public static boolean checkCourseMax(final Course c, final CourseSlot s) {
-        return (s.getCourseCount() + 1 < s.getCourseMax());
-
+    public static boolean checkCourseMax(Course c, CourseSlot s) {
+        return (s.getCourseCount() + 1 <= s.getCourseMax());
     }
 
     /**
-     *
+     *Not sure what it does so I won't touch it
+     *Make sure 2 lectures of the same course aren't in the same slot?
      * @param c
      * @param s
      * @return true if no hard constraint broken
      */
-    public static boolean checkDuplicateCourse(final Course c, final CourseSlot s) {
+    public static boolean checkDuplicateCourse(Course c, CourseSlot s) {
         // Go through courses in slot to see if theres a duplicate
         // Right now its a runthrough but ideally once its implemented correctly it
         // should be a lookup??????????????
-        boolean result = true;
         final ArrayList<Course> lookup = s.getAssignedCourses();
         for (int i = 0; i < lookup.size(); i++) {
             if (lookup.get(i).getCourseNum() == c.getCourseNum()
                     && lookup.get(i).getCourseType().equals(c.getCourseType())) {
-                result = false;
+                return false;
             }
         }
-
-        return result;
-
+        return true;
     }
 
-    public static boolean checkEveningClasses(final Course c, final CourseSlot s) {
-        boolean toReturn = true;
+    public static boolean checkEveningClasses(Course c, CourseSlot s) {
         if (Integer.toString(c.getLectureNum()).substring(0, 1).equals("9")) {
             // Check evening class
-            toReturn = s.getTime() >= 1800;
+            return s.getTime() >= 1800;
         }
-        return toReturn;
-
+        return true;
     }
 
     public static boolean checkConflict500Levels(final Course c, final CourseSlot s) {
-        boolean result = true;
         if (c.getCourseNum() >= 500) {
             final ArrayList<Course> lookup = s.getAssignedCourses();
             for (int i = 0; i < lookup.size(); i++) {
                 if (lookup.get(i).getCourseNum() >= 500) {
-                    result = false;
-                    break;
+                    return false;
                 }
             }
         }
-        return result;
+        return true;
     }
 
-    public static boolean checkLabConflicts(final Course c, final CourseSlot s, final HashMap<Integer, Slot> MWFLec,
-            final HashMap<Integer, Slot> TuThLec, final HashMap<Integer, Slot> MWLab,
-            final HashMap<Integer, Slot> TuThLab, final HashMap<Integer, Slot> FLab) {
-
+    public static boolean checkConflictsWithSameName(Unit c, Slot s) {
         for(Slot slot : s.getOverlaps()){
             for(Unit unit : slot.getClassAssignment()){
                 if(unit.getCourseNum() == c.getCourseNum() && unit.getCourseType().equals(c.getCourseType())){
@@ -90,11 +77,11 @@ public class HardConstrainsts {
             final HashMap<Integer, Slot> MWLab, final HashMap<Integer, Slot> TuThLab,
             final HashMap<Integer, Slot> FLab) {
         return checkLabMax(lab, slot) && checkDuplicateLab(lab, slot)
-                && checkCourseConflicts(lab, slot, MWFLec, TuThLec, MWLab, TuThLab, FLab);
+                && checkConflictsWithSameName(lab, slot);
     }
 
     public static boolean checkLabMax(final Lab c, final LabSlot s) {
-        return (s.getLabCount() + 1 < s.getLabMax());
+        return (s.getLabCount() + 1 <= s.getLabMax());
     }
 
     public static boolean checkDuplicateLab(final Lab c, final LabSlot s) {
@@ -110,20 +97,6 @@ public class HardConstrainsts {
         }
         return result;
 
-    }
-
-    public static boolean checkCourseConflicts(final Lab c, final LabSlot s, final HashMap<Integer, Slot> MWFLec,
-            final HashMap<Integer, Slot> TuThLec, final HashMap<Integer, Slot> MWLab,
-            final HashMap<Integer, Slot> TuThLab, final HashMap<Integer, Slot> FLab) {
-
-        for(Slot slot : s.getOverlaps()){
-            for(Unit unit : slot.getClassAssignment()){
-                if(unit.getCourseNum() == c.getCourseNum() && unit.getCourseType().equals(c.getCourseType())){
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 
 
