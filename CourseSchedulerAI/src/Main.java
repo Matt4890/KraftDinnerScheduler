646d05b007edf290ee819c9f613e94dddc21bd8d
kaitlin.decf <kaitlin.decf@gmail.com>
2019-11-24 12:31:05 -0700

    Redirected Print Stream to File

--- CourseSchedulerAI/src/Main.java
+++ CourseSchedulerAI/src/Main.java
@@ -3,9 +3,19 @@ import schedule.*;
 import tree.Generator;
 import coursesULabs.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-  public static void main(String[] args) {
+  public static void main(String[] args) throws FileNotFoundException{
+    //Instantiating the File class
+    File file = new File("output.txt");
+    //Instantiating the PrintStream class
+    PrintStream stream = new PrintStream(file);
+    System.out.println("From now on "+file.getAbsolutePath()+" will be your console");
+    System.setOut(stream);
+    //Printing values to file
+    System.out.println("Hello, how are you");
+    System.out.println("Welcome to Tutorialspoint");
 
     String filename;
     int courseMinPen;
